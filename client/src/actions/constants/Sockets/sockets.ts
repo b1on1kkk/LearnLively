c565b2f0dc5ae5a8c0481b7eec6d2890b641310f
b1on1kkk <b1on1kkk@mail.ru>
2024-06-14 02:56:37 +0300

    prepair front to production

--- client/src/actions/constants/Sockets/sockets.ts
+++ client/src/actions/constants/Sockets/sockets.ts
@@ -1,4 +1,8 @@
 export const SOCKETS_ROOT = {
-  service_logic_socket: "http://localhost:3001/service_logic",
-  chat_socket: "http://localhost:3001/chat"
+  service_logic_socket: "https://learnlively-server.onrender.com/service_logic",
+  chat_socket: "https://learnlively-server.onrender.com/chat"
 };
+
+// development: http://localhost:3001
+
+// production links: https://learnlively-server.onrender.com/
