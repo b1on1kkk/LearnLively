3574356db1bba38b5197a567e302928c00fe1574
b1on1kkk <b1on1kkk@mail.ru>
2024-06-14 12:42:46 +0300

    ports were changed

--- client/src/actions/constants/Sockets/sockets.ts
+++ client/src/actions/constants/Sockets/sockets.ts
@@ -1,6 +1,7 @@
 export const SOCKETS_ROOT = {
-  service_logic_socket: "https://learnlively-server.onrender.com/service_logic",
-  chat_socket: "https://learnlively-server.onrender.com/chat"
+  service_logic_socket:
+    "https://learnlively-server2.onrender.com/service_logic",
+  chat_socket: "https://learnlively-server2.onrender.com/chat"
 };
 
 // development: http://localhost:3001
